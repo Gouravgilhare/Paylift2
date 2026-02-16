a5931b74fa446e8835ec8ea2d1228f12d6b12d02
Gouravgilhare <gouravgilhare4@gmail.com>
2026-02-16 23:47:21 +0530

    adding nginx in docker

--- modules/maps/routes/maps.routes.js
+++ modules/maps/routes/maps.routes.js
@@ -1,12 +1,16 @@
 // modules/location/routes/maps.routes.js
 import express from "express";
 import * as mapsController from "../controllers/maps.controller.js";
-
+import { verifyToken } from "../../auth/middleware/auth.middleware.js";
 const router = express.Router();
 
-router.post("/route", mapsController.getRoute);
-router.post("/distance-matrix", mapsController.getDistanceMatrix);
-router.get("/places/autocomplete", mapsController.placeAutocomplete);
-router.get("/places/details", mapsController.getPlaceDetails);
+router.post("/route", verifyToken, mapsController.getRoute);
+router.post("/distance-matrix", verifyToken, mapsController.getDistanceMatrix);
+router.get(
+  "/places/autocomplete",
+  verifyToken,
+  mapsController.placeAutocomplete,
+);
+router.get("/places/details", verifyToken, mapsController.getPlaceDetails);
 
 export default router;
